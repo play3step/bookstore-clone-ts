a26f63939fc8f49c4817c6783d9302d39c665a8a
echo <play3step@naver.com>
2024-11-14 15:34:00 +0900

    feat: 버튼 컴포넌트 생성

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,13 @@
+import Button from "../components/common/Button";
 import Title from "../components/common/Title";
 
 const Home = () => {
   return (
     <div>
       <Title size="small">제목테스트</Title>
+      <Button size="large" scheme="primary">
+        버튼 테스트
+      </Button>
     </div>
   );
 };
--- src/style/theme.ts
+++ src/style/theme.ts
@@ -3,6 +3,8 @@ export type ThemeName = "light" | "dark";
 export type HeadingSize = "large" | "medium" | "small";
 
 export type ColorKey = "primary" | "background" | "secondary" | "third";
+export type ButtonSize = "large" | "medium" | "small";
+export type ButtonScheme = "primary" | "normal";
 
 interface theme {
   name: ThemeName;
@@ -15,6 +17,21 @@ interface theme {
     [key in HeadingSize]: {
       fontSize: string;
     };
+  };
+  button: {
+    [key in ButtonSize]: {
+      fontSize: string;
+      padding: string;
+    };
+  };
+  buttonScheme: {
+    [key in ButtonScheme]: {
+      color: string;
+      backgroundColor: string;
+    };
+  };
+  borderRadius: {
+    default: string;
   };
 }
 
@@ -36,6 +53,33 @@ export const light: theme = {
     small: {
       fontSize: "1rem",
     },
+  },
+  button: {
+    large: {
+      fontSize: "1.5rem",
+      padding: "1rem 2rem",
+    },
+    medium: {
+      fontSize: "1rem",
+      padding: "0.5rem 1rem",
+    },
+    small: {
+      fontSize: "0.75rem",
+      padding: "0.25rem 0.5rem",
+    },
+  },
+  buttonScheme: {
+    primary: {
+      color: "white",
+      backgroundColor: "midnightblue",
+    },
+    normal: {
+      color: "black",
+      backgroundColor: "lightgray",
+    },
+  },
+  borderRadius: {
+    default: "4px",
   },
 };
 
